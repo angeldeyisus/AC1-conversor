02490e59dd64f9553c4edee824d4327973897bff
Angel Santos <93794342+angeldeyisus@users.noreply.github.com>
2025-09-03 08:50:22 -0800

    Subida de proyecto

---
+++ Conversor_AngelSantos/app/src/main/java/santos/angel/conversor/MainActivity.kt
@@ -0,0 +1,17 @@
+package santos.angel.conversor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var valor = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
